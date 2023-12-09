f4a94e7a302d5875ad19b9081e0c62b3575ec5b5
simplilearn github <danayswrya@gmail.com>
2023-12-09 15:51:52 +0800

    first commit

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,8 +5,9 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
+$routes->setAutoRoute(true);
 $routes->get('/', 'Home::index');
 $routes->get('/about', 'Page::about');
 $routes->get('/contact', 'Page::contact');
 $routes->get('/faqs', 'Page::faqs');
-$routes->setAutoRoute(true);
+$routes->get('/news', 'News::index');
